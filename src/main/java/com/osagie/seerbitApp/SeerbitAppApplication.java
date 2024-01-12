4c77f404b7c0cf41e105d3bec3231fb763a4d705
Osagie Erhabor <erhaborosagie@gmail.com>
2024-01-12 07:20:51 +0100

    Initial BootStrap

---
+++ src/main/java/com/osagie/seerbitApp/SeerbitAppApplication.java
@@ -0,0 +1,13 @@
+package com.osagie.seerbitApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeerbitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeerbitAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osagie/seerbitApp/SeerbitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osagie.seerbitApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeerbitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
